7452dfe78f70ed0e88b94e54dc33bfa37e96f9af
NCupid05 <trinicruzball@icloud.com>
2026-04-28 16:12:21 -0400

    Updated crimeLosAngeles.R and added data visualization

--- crimeLosAngeles.R
+++ crimeLosAngeles.R
@@ -63,15 +63,26 @@ crimeHollywood_filtered <- crimeHollywood_filtered %>%
 crimeHollywood_filtered <- crimeHollywood_filtered %>%
   rename(Crime_Category = crime_category)
 
-##Victim Demographic: Table and Box Plots
-victim_table <- crimeHollywood_filtered %>%
-  select(Victim_Age, Victim_Sex)
+crimeHollywood_filtered <- crimeHollywood_filtered %>%
+  filter(Victim_Sex %in% c("M", "F")) %>%
+  mutate(
+    Victim_Sex = recode(Victim_Sex,
+                        "M" = "Male",
+                        "F" = "Female")
+  )
+
+#Victim Demographic Data Visualization
 
 library(ggplot2)
 
-ggplot(victim_table, aes(x = Victim_Sex, y = Victim_Age)) + 
-  geom_boxplot() + 
-  labs (title = "Victim Age Distribution by Sex",
-        x = "Sex",
-        y = "Age") + 
+victim_table <- crimeHollywood_filtered %>%
+  select(Year, Victim_Age, Victim_Sex)    # Filters out every column besides year, victim age, and victim sex
+
+
+ggplot(victim_table, aes(x = Victim_Sex, y = Victim_Age, fill = factor(Year))) + 
+  geom_boxplot(position = position_dodge(width = 0.8)) + #creates box plot with sex, age, and categorized years
+  labs (title = "Victim Age Distribution by Sex",   #title of box plot
+        x = "Sex",    # Sex x axis for box plot
+        y = "Age",    # Age y axis for box plot
+        fill = "Year") +     # Legend for the title
   theme_minimal()
